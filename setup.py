0eeca0807f05e527a8cbaca3f6c10abb82eb6038
napuzba <kobi@napuzba.com>
2015-12-10 15:03:25 +0200

    update package info

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   description = 'Downloading files (http,ftp). Supports cachinhg and allows uniform access to remote and local files',
   author = 'napuzba',
   author_email = 'kobi@napuzba.com',
-  url = 'https://github.com/napuzba/FileLoader.git',
+  url = 'https://github.com/napuzba/fileloader.git',
   download_url = 'https://github.com/napuzba/fileloader/archive/0.1.zip',
   keywords = ['download','ftp','http'],
   classifiers = [],
