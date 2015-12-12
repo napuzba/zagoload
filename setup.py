5c143adb5d292408ba638f26a8f1b7eecc0b5239
napuzba <kobi@napuzba.com>
2015-12-12 20:48:13 +0200

    First release

--- setup.py
+++ setup.py
@@ -1,14 +1,52 @@
 from distutils.core import setup
 
+description = 'Downloading files (http,ftp). Supports: cachinhg, uniform access to remote and local files'
+long_description = '''
+=====
+About
+=====
+
+fileloader module simplifies downloading and accessing remote files. 
+
+********
+Features
+********
+
+1. Allows uniform access to Remote files ( accessible thought HTTP and FTP protocols) and local files.
+2. Support caching
+
+*****
+Usage
+*****
+
+See `Download Files with Fileloader`_ for turorial.
+
+.. _Download Files with Fileloader: http://www.napuzba.com/story/download-files-with-fileloader/'
+'''
+
 setup(
-  name = 'fileloader',
-  packages = ['fileloader'], 
-  version = '0.1',
-  description = 'Downloading files (http,ftp). Supports cachinhg and uniform access to remote and local files',
-  author = 'napuzba',
-  author_email = 'kobi@napuzba.com',
-  url = 'https://github.com/napuzba/fileloader.git',
-  download_url = 'https://github.com/napuzba/fileloader/archive/0.1.zip',
-  keywords = ['download','ftp','http'],
-  classifiers = [],
+  name             = 'fileloader',
+  packages         = ['fileloader'], 
+  version          = '1.0',  
+  author           = 'napuzba',
+  author_email     = 'kobi@napuzba.com',
+  url              = 'https://github.com/napuzba/fileloader.git',
+  download_url     = 'https://github.com/napuzba/fileloader/archive',
+  description      = description,
+  long_description = long_description,
+  keywords         = ['download,crawl,ftp,http'],
+  classifiers      = [
+	'Development Status :: 5 - Production/Stable',
+	'Intended Audience :: Developers',
+	'Intended Audience :: Developers',
+	'Intended Audience :: Information Technology',
+	'Intended Audience :: Science/Research',
+	'Intended Audience :: System Administrators',
+	'Programming Language :: Python :: 2',
+	'Programming Language :: Python :: 3',
+	'Operating System :: OS Independent',
+	'Topic :: Software Development :: Libraries :: Python Modules',
+	'Topic :: Internet :: File Transfer Protocol (FTP)',	
+	'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
+  ],
 )
