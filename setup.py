21691f60579d601646d1fd9a97759cf9cbdb7c61
napuzba <kobi@napuzba.com>
2016-09-29 01:36:25 -0700

    version 2.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup
 
-description = 'Download files (http,ftp).Supports: cachinhg, uniform access to remote and local files'
+description = 'Download files (http,ftp). Supports: cachinhg, uniform access to remote and local files'
 long_description = '''
 =====
 About
@@ -29,11 +29,11 @@ See `Download Files with Fileloader`_ for turorial.
 
 setup(
   name             = 'fileloader',
-  packages         = ['fileloader'], 
+  packages         = ['fileloader'],
   install_requires = [
      'urllib3 >= 1.0',
   ],
-  version          = '2.1.1',  
+  version          = '2.1.1',
   author           = 'napuzba',
   author_email     = 'kobi@napuzba.com',
   url              = 'https://github.com/napuzba/fileloader.git',
@@ -44,7 +44,7 @@ setup(
   keywords         = ['download,crawl,ftp,http'],
   classifiers      = [
     'Topic :: Software Development :: Libraries :: Python Modules',
-    'Topic :: Internet :: File Transfer Protocol (FTP)',    
+    'Topic :: Internet :: File Transfer Protocol (FTP)',
     'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
 
     'Development Status :: 5 - Production/Stable',
